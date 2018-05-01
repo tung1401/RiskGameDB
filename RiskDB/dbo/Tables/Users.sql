c9adc418e9a47e2410872b1edf039fd67d12ee61
patompoltsj@gmail.com <patompoltsj@gmail.com>
2018-05-01 18:46:45 +0700

    update

--- RiskDB/dbo/Tables/Users.sql
+++ RiskDB/dbo/Tables/Users.sql
@@ -2,7 +2,7 @@
     [UserId]    INT            IDENTITY (1, 1) NOT NULL,
     [FirstName] NVARCHAR (MAX) NULL,
     [LastName]  NVARCHAR (MAX) NULL,
-	[Email]  NVARCHAR (MAX) NULL,
+	[Email]  NVARCHAR (MAX) NULL, 
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
 );
 
