5105a4b38c20bd72bc30f9a7043cd4eb831fe2c8
PATOMPOL\KwangTung <patompoltsj@gmail.com>
2019-04-02 23:08:23 +0700

    update db

--- RiskDB/dbo/Tables/GameBattle.sql
+++ RiskDB/dbo/Tables/GameBattle.sql
@@ -9,6 +9,7 @@
     [ActionEffectValue] INT NULL,
     CONSTRAINT [PK_GameBattle] PRIMARY KEY CLUSTERED ([GameBattleId] ASC),
     CONSTRAINT [FK_GameBattle_GameRoom] FOREIGN KEY ([GameRoomId]) REFERENCES [dbo].[GameRoom] ([GameRoomId]),
-    CONSTRAINT [FK_GameBattle_Risk] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[Risk] ([RiskId])
+    CONSTRAINT [FK_GameBattle_Risk] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[Risk] ([RiskId]),
+	CONSTRAINT [FK_GameBattle_RiskOption] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[RiskOption] ([RiskOptionId])
 );
 
--- RiskDB/dbo/Tables/UserGameRisk.sql
+++ RiskDB/dbo/Tables/UserGameRisk.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[UserGameRisk] (
     [UserGameRiskId] INT IDENTITY (1, 1) NOT NULL,
     [RiskId]     INT NOT NULL,
-	[OptionId]     INT NOT NULL,
+	[RiskOptionId]     INT NOT NULL,
     [UserId]           INT NOT NULL,
     [GameRoomId]             INT NOT NULL,
     [Turn] INT NULL, 
@@ -9,6 +9,8 @@
     [CreateBy] INT NULL, 
     CONSTRAINT [PK_UserGameRisk] PRIMARY KEY CLUSTERED ([UserGameRiskId] ASC),
     CONSTRAINT [FK_UserGameRisk_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId]),
-	CONSTRAINT [FK_UserGameRisk_GameRoom] FOREIGN KEY ([GameRoomId]) REFERENCES [dbo].[GameRoom] ([GameRoomId])
+	CONSTRAINT [FK_UserGameRisk_GameRoom] FOREIGN KEY ([GameRoomId]) REFERENCES [dbo].[GameRoom] ([GameRoomId]),
+	CONSTRAINT [FK_UserGameRisk_Risk] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[Risk] ([RiskId]),
+	CONSTRAINT [FK_UserGameRisk_RiskOption] FOREIGN KEY ([RiskOptionId]) REFERENCES [dbo].[RiskOption] ([RiskOptionId])
 );
 
