57e9ab0dd716d6be6c87fcb0979585e194b154cd
PATOMPOL\KwangTung <patompoltsj@gmail.com>
2019-04-18 17:26:45 +0700

    update db

--- RiskDB/dbo/Tables/GameBattle.sql
+++ RiskDB/dbo/Tables/GameBattle.sql
@@ -6,10 +6,13 @@
     [Ratio]             INT            NULL,
     [Turn]              INT            NULL,
     [ActionEffectType]  INT            NULL,
-    [ActionEffectValue] INT NULL,
+    [ActionEffectValue] INT			   NULL,
+	[RiskNewsId]		INT			   NULL,
+
     CONSTRAINT [PK_GameBattle] PRIMARY KEY CLUSTERED ([GameBattleId] ASC),
     CONSTRAINT [FK_GameBattle_GameRoom] FOREIGN KEY ([GameRoomId]) REFERENCES [dbo].[GameRoom] ([GameRoomId]),
     CONSTRAINT [FK_GameBattle_Risk] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[Risk] ([RiskId]),
-	CONSTRAINT [FK_GameBattle_RiskOption] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[RiskOption] ([RiskOptionId])
+	CONSTRAINT [FK_GameBattle_RiskOption] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[RiskOption] ([RiskOptionId]),
+	CONSTRAINT [FK_GameBattle_RiskNews] FOREIGN KEY ([RiskNewsId]) REFERENCES [dbo].[RiskNews] ([RiskNewsId]),
 );
 
---
+++ RiskDB/dbo/Tables/RiskNews.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[RiskNews] (
+    [RiskNewsId]           INT    IDENTITY (1, 1) NOT NULL,
+	[RiskId]               INT    NOT NULL,
+    [RiskNewsDetail]       NVARCHAR (MAX) NULL,
+    [RiskNewsProbability]  INT NULL, 
+    [RiskNewsImpact]	   INT NULL,
+
+    CONSTRAINT [PK_RiskNews] PRIMARY KEY CLUSTERED ([RiskNewsId] ASC),
+	CONSTRAINT [FK_RiskNews_Risk] FOREIGN KEY ([RiskId]) REFERENCES [dbo].[Risk] ([RiskId])
+);
+
